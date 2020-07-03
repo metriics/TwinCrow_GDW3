4357ef14fec458d0b992e3bcc7f12061922e9063
Jonah Griffin <jonah.griffin@uoit.net>
2020-07-03 13:10:44 -0400

    Added base character class with enemy class child
    Added things such as name, description, state, health, dmg, armour, etc

---
+++ Dustbound/Assets/Base Classes/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public CharacterStates state;
+    public string charName;
+    public string charDesc;
+    public int baseHealth;
+    public int baseDmg;
+    public int baseArmour;
+
+    public virtual void Attack()
+    {
+        //Overide this method for specific characters
+    }
+
+    public enum CharacterStates { idle, attacking, walking }
+}
